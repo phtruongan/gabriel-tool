a624ca686ba13637fbce03b853500c69b6900461
phtruongan <phtruongan@gmail.com>
2019-04-04 10:06:03 +0300

    Updates

---
+++ precache-manifest.b59874fc96ffefe0654e932858f56e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e356c722a8dca47e2da5",
+    "url": "/gabriel-tool/static/css/main.d33fbe99.chunk.css"
+  },
+  {
+    "revision": "e356c722a8dca47e2da5",
+    "url": "/gabriel-tool/static/js/main.e356c722.chunk.js"
+  },
+  {
+    "revision": "1fd612dfaee9c7984291",
+    "url": "/gabriel-tool/static/css/1.d91e8587.chunk.css"
+  },
+  {
+    "revision": "1fd612dfaee9c7984291",
+    "url": "/gabriel-tool/static/js/1.1fd612df.chunk.js"
+  },
+  {
+    "revision": "a60933583952a9695017",
+    "url": "/gabriel-tool/static/js/runtime~main.a6093358.js"
+  },
+  {
+    "revision": "08d109f37aff027e05c11ff1aa0e867f",
+    "url": "/gabriel-tool/index.html"
+  }
+];
